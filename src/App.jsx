2017ca6b13c63f2705597b4305af2c83006de730
ibrahimahtsham <ibrahimahtsham2002@gmail.com>
2025-12-09 06:38:27 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <h1>bruh</h1>
+      <Button variant="contained" color="primary">
+        MUI Button
+      </Button>
+    </>
+  );
+}
+
+export default App;
